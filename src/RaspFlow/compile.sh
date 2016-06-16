a8948cafba7044966def8ec30ff54a47410b35a4
MrSlayer02 <louiscaubet@outlook.com>
2016-06-16 14:37:14 +0200

    Create compile.sh

---
+++ src/RaspFlow/compile.sh
@@ -0,0 +1,2 @@
+sudo mkdir -p ./bin
+sudo javac -d ./bin -classpath .:classes:./bin/GopigoAPI.jar ./src/com/icloud/louiscaubet/raspflow/*.java ./src/com/icloud/louiscaubet/raspflow/api/*.java ./src/com/icloud/louiscaubet/raspflow/objecttasks/*.java ./src/com/icloud/louiscaubet/raspflow/tensorflow/*.java
